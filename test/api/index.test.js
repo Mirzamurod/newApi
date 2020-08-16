574330d758e051b9d8ea422885a22bd0b14740e8
Mirzamurod <mirzamurodrahimberdiyev@gmail.com>
2020-08-16 23:15:46 +0500

    xato togillandi lekin oxirgachamas

--- test/api/index.test.js
+++ test/api/index.test.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
-const server = require('./../../app');
+const server = require('../../app');
 
 chai.use(chaiHttp);
 
-describe('Bosh sahifa Testi', () => {
-    it('(GET /) bosh sahifani korsatish kerak', done => {
+describe('Bosh Sahifa Testi', () => {
+    it('(GET /) bu bosh sahifani korsatishi kerak', done => {
         chai.request(server)
             .get('/')
             .end((err, res) => {
